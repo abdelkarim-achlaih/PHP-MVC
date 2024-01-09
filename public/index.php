807d0b37af03a438d2c0ccac092d24b9d1d5327f
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-01-09 17:35:42 +0100

    Adding Routing System

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo 'hello World';
+function show ($item) {
+  echo '<pre>';
+  print_r($item);
+  echo '</pre>';
+}
+function splitURL () {
+  $url = $_GET['url'] ?? 'home';
+  $url = explode('/', $url);
+  return $url;
+}
+function loadController () {
+  $url = splitURL();
+  $filename = "../app/controllers/".ucfirst($url[0]).".php";
+  if (file_exists($filename)) {
+    require $filename;
+  } else {
+    $filename = "../app/controllers/_404.php";
+    require $filename;
+  }
+}
+loadController();
